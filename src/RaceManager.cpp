5feb446f2531c94a0c90e078ddf89e46d74e3430
Mini <mini@minizbot.org>
2026-04-15 05:06:57 -0400

    Ignore original DLC1VampireBeastRace in generator

--- src/RaceManager.cpp
+++ src/RaceManager.cpp
@@ -29,7 +29,7 @@ namespace MPL::Managers
             for (auto frm : races)
             {
                 auto race = frm->As<RE::TESRace>();
-                if (race->keywords != nullptr && race->HasKeywordString("Vampire") && !race->HasKeywordString("VampireLord") && !race->HasKeywordString("HVL_Ignore"))
+                if (race->keywords != nullptr && race->HasKeywordString("Vampire") && !race->HasKeywordString("VampireLord") && !race->HasKeywordString("HVL_Ignore") && race != this->OriginalVL)
                 {
                     auto form = cfc->Create();
                     auto edid = std::format("{}Lord", race->GetFormEditorID());
